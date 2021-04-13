be03d82e762e3ae0d131d39cf782cc5f6a5e2630
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-13 14:31:04 -0300

    Create GameManager prefab & script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager _instanceGameManager;
+
+    private void Awake()
+    {
+        if (_instanceGameManager == null)
+        {
+            _instanceGameManager = this;
+        }
+        else if (_instanceGameManager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void StartCorrutineLoadScene(string sceneTittle)
+    {
+        StartCoroutine(sceneTittle);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Application.Quit();
+    }
+}
