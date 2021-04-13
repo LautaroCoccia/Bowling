74c6c84694c0c59dedbf68674c5dda97f035507c
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-13 16:25:19 -0300

    BallController script

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float force = 0.0f;
+    private Rigidbody BallRidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BallRidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BallRidbody.AddForce(Vector3.forward * force * Time.deltaTime);
+    }
+}
