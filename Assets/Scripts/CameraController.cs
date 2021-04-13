7670499960b80e8dd48fa689a67964828490296f
LautaroCoccia <lautaroauditore@gmail.com>
2021-04-13 16:28:26 -0300

    Camera controller script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform ball;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = ball.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        //transform.LookAt(ball);
+    }
+}
